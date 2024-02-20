6e8b16e7fd36577f66d2a5438e2f7960fcaeca21
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-02-20 10:24:21 +0100

    app head

--- app.vue
+++ app.vue
@@ -1,20 +1,3 @@
 <template>
   <div>Nuxt app test</div>
 </template>
-
-<script>
-useHead({
-  title: "Vease",
-  meta: [
-    { charset: "utf-8" },
-    { name: "viewport", content: "width=device-width, initial-scale=1" },
-    {
-      hid: "description",
-      name: "description",
-      content:
-        "Technology company tackling next generation of modeling challenges using advanced meshing",
-    },
-  ],
-  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-});
-</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,21 @@ export default defineNuxtConfig({
     ],
   },
 
+  app: {
+    head: {
+      titleTemplate: "Vease",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Platform for geological data visualization",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/ico", href: "/icon_share_twin.ico" }],
+    },
+  },
   devtools: {
     enabled: process.env.NODE_ENV === "production" ? false : true,
   },
