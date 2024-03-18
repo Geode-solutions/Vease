8cb47a0761eec587f4df30878e5ef08580b693d7
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-03-18 20:29:34 +0100

    code cleanup

--- electron/main.js
+++ electron/main.js
@@ -5,19 +5,12 @@ import path from "path";
 updateElectronApp();
 
 process.env.ROOT = path.join(__dirname, "..", "..");
-console.log("ROOT", process.env.ROOT);
 process.env.DIST = path.join(process.env.ROOT, "dist-electron");
-console.log("DIST", process.env.DIST);
 process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
   ? path.join(process.env.ROOT, "public")
   : path.join(process.env.ROOT, ".output/public");
-console.log("process.resourcesPath", process.resourcesPath);
-
-const distPath = path.join(__dirname, "../.output/public");
-console.log("distPath", distPath);
 
 process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
-console.log("VITE_PUBLIC", process.env.VITE_PUBLIC);
 
 const preload = path.join(process.env.DIST, "preload.js");
 
@@ -35,11 +28,8 @@ app.whenReady().then(() => {
     },
   });
   if (app.isPackaged) {
-    console.log("APP IS PACKAGED");
     win.loadFile("../public/index.html");
   } else {
-    console.log("APP NOT PACKAGED");
-    console.log("VITE_DEV_SERVER_URL", process.env.VITE_DEV_SERVER_URL);
     win.loadURL(process.env.VITE_DEV_SERVER_URL);
     win.webContents.openDevTools();
   }
