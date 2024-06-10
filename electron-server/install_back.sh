a623e9ccb8e720ab43e02ac9302c507b1adc4372
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-10 16:45:50 +0200

    fix pyinstaller script

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -5,4 +5,6 @@ python3 -m venv venv_back
 ./venv_back/bin/pip install -r ./electron-server/requirements_back.txt
 ./venv_back/bin/pip install pyinstaller
 source ./venv_back/bin/activate
-pyinstaller --onedir --collect-datas opengeodeweb_back --paths ./venv_back/lib/python3.10/site-packages ./venv_back/bin/geodeapp_server
+./venv_back/bin/pyinstaller --onefile --collect-all opengeodeweb_back ./venv_back/bin/geodeapp_server
+
+
