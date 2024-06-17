a91be52c3e22f92d4d6302d663f8adf1e1960c47
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-17 10:39:42 +0200

    Update install script

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -1,10 +1,26 @@
-rm -rf venv_back dist_back build
-python3 -m venv venv_back
-./venv_back/bin/pip install pip-tools
-./venv_back/bin/python -m pip-compile ./electron-server/requirements_back.in
-./venv_back/bin/pip install -r ./electron-server/requirements_back.txt
-./venv_back/bin/pip install pyinstaller
-source ./venv_back/bin/activate
-./venv_back/bin/pyinstaller --onefile --collect-all opengeodeweb_back ./venv_back/bin/geodeapp_back --distpath ./dist_back
+rm -rf venv_back ./electron-server/dist_back ./electron-server/build
+wait
+echo 'Removing previous bundles'
+python3 -m venv ./electron-server/venv_back
+echo 'Creating venv'
+wait
+./electron-server/venv_back/bin/pip install pip-tools
+echo 'Installing pip-tools'
+wait
+./electron-server/venv_back/bin/python -m pip-compile ./electron-server/requirements_back.in
+echo 'Compiling requirements'
+wait
+./electron-server/venv_back/bin/pip install -r ./electron-server/requirements_back.txt
+echo 'Installing requirements'
+wait
+./electron-server/venv_back/bin/pip install pyinstaller
+echo 'Installing PyInstaller'
+wait
+source ./electron-server/venv_back/bin/activate
+echo 'Activating venv'
+wait
+./electron-server/venv_back/bin/pyinstaller --onefile --collect-all opengeodeweb_back ./electron-server/venv_back/bin/geodeapp_back --distpath ./electron-server/dist-back
+echo 'Bundling geodeapp_back'
+wait
 
 
