e9fd4d86bb0301c8e2ccfd0e7a6100a3232c1ad5
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-17 09:23:10 +0200

    remove log

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-console.log("_____TEST ELECTRON PRELOAD_______");
-
 contextBridge.exposeInMainWorld("electronAPI", {
   run_back: async (port) => {
     const result = await ipcRenderer.invoke("run_back", port);
