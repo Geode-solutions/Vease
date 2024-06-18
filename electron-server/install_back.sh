29acbee45d0d73d62ee3b49d76d6fbd6fbb2a48d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-18 12:38:10 +0200

    fix(CICD): test variable

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 rm -rf ./electron-server/venv_back ./electron-server/dist_back ./electron-server/build ./electron-server/requirements_back.txt
-python3 -m venv ./electron-server/venv_back
-source ./electron-server/venv_back/bin/activate
-pip install pip-tools
-pip-compile ./electron-server/requirements_back.in
-pip install -r ./electron-server/requirements_back.txt
-pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back ./electron-server/venv_back/bin/geodeapp_back --distpath ./electron-server/dist_back
+folder=./electron-server/venv_back
+python3 -m venv $folder
+bin_folder=$folder/bin
+# source $folder/bin/activate
+$bin_folder/pip install pip-tools
+$bin_folder/pip-compile ./electron-server/requirements_back.in
+$bin_folder/pip install -r ./electron-server/requirements_back.txt
+$bin_folder/pip install pyinstaller
+$bin_folder/pyinstaller --onefile --collect-data opengeodeweb_back $bin_folder/geodeapp_back --distpath ./electron-server/dist_back
