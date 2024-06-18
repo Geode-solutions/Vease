2b8bd224036e61c7a306a3191d535dc00a733b14
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-18 14:00:39 +0200

    fix(CICD): test back & viewer

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-echo "TEST"
 rm -rf electron-server/venv_back electron-server/dist_back electron-server/build electron-server/requirements_back.txt
-echo "Remove"
 folder=electron-server/venv_back
 python3 -m venv $folder
-echo "Activate venv"
 bin_folder=$folder/bin
 $bin_folder/pip install pip-tools
-echo "Activate venv"
 $bin_folder/pip-compile electron-server/requirements_back.in
-echo "Compile requirements"
 $bin_folder/pip install -r electron-server/requirements_back.txt
-echo "Install requirements"
 $bin_folder/pip install pyinstaller
-echo "Install PyInstaller"
 $bin_folder/pyinstaller --onefile --collect-data opengeodeweb_back $bin_folder/geodeapp_back --distpath electron-server/dist_back
-echo "Generate bundle"
--- electron-server/install_viewer.sh
+++ electron-server/install_viewer.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-rm -rf ./electron-server/venv_viewer ./electron-server/dist_viewer ./electron-server/build ./electron-server/requirements_viewer.txt
-python3 -m venv ./electron-server/venv_viewer
-source ./electron-server/venv_viewer/bin/activate
-pip install pip-tools
-pip-compile ./electron-server/requirements_viewer.in
-pip install -r ./electron-server/requirements_viewer.txt
-pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules ./electron-server/venv_viewer/bin/geodeapp_viewer --distpath ./electron-server/dist_viewer
+rm -rf electron-server/venv_viewer electron-server/dist_back electron-server/build electron-server/requirements_viewer.txt
+folder=electron-server/venv_viewer
+python3 -m venv $folder
+bin_folder=$folder/bin
+$bin_folder/pip install pip-tools
+$bin_folder/pip-compile electron-server/requirements_viewer.in
+$bin_folder/pip install -r electron-server/requirements_viewer.txt
+$bin_folder/pip install pyinstaller
+$bin_folder/pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $bin_folder/geodeapp_viewer --distpath electron-server/dist_viewer
