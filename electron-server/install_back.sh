8e56d956dbab657baa6b07d5ec535e1083d8ca0d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-19 11:21:54 +0200

    fix(CICD): remove if os

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -3,16 +3,10 @@ set -e
 rm -rf electron-server/venv_back electron-server/dist_back electron-server/build electron-server/requirements_back.txt
 folder=electron-server/venv_back
 python3 -m venv $folder
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    bin_folder=$folder/bin
-    source $bin_folder/activate
-elif [[ "$OSTYPE" == "msys" ]]; then
-    bin_folder=$folder/Scripts
-    $bin_folder/activate.bat
-fi
+bin_folder=$folder/bin
+source $bin_folder/activate
 pip install pip-tools
 pip-compile electron-server/requirements_back.in
 pip install -r electron-server/requirements_back.txt
 pip install pyinstaller
-ls -R electron-server/venv_back
 pyinstaller --onefile --collect-data opengeodeweb_back $bin_folder/geodeapp_back --distpath electron-server/dist_back
--- electron-server/install_viewer.sh
+++ electron-server/install_viewer.sh
@@ -3,16 +3,10 @@ set -e
 rm -rf electron-server/venv_viewer electron-server/dist_back electron-server/build electron-server/requirements_viewer.txt
 folder=electron-server/venv_viewer
 python3 -m venv $folder
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    bin_folder=$folder/bin
-    source $bin_folder/activate
-elif [[ "$OSTYPE" == "msys" ]]; then
-    bin_folder=$folder/Scripts
-    $bin_folder/activate.bat
-fi
+bin_folder=$folder/bin
+source $bin_folder/activate
 pip install pip-tools
 pip-compile electron-server/requirements_viewer.in
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
-ls -R electron-server/venv_viewer
 pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $bin_folder/geodeapp_viewer --distpath electron-server/dist_viewer
