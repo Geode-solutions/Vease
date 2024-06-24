f8e042877e82d2481e2dbc556409db9bdd5798f4
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-24 14:00:20 +0200

    fix(CICD): test CICD

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -5,8 +5,9 @@ folder=electron-server/venv_back
 python3 -m venv $folder
 bin_folder=$folder/bin
 source $bin_folder/activate
+packages_path=$folder/lib/python3.10/site-packages
 pip install pip-tools
 pip-compile electron-server/requirements_back.in
 pip install -r electron-server/requirements_back.txt
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back $bin_folder/geodeapp_back --distpath electron-server/dist_back
+pyinstaller --onefile --collect-data opengeodeweb_back $packages_path/geodeapp_back/app.py --distpath electron-server/dist_back -n geodeapp_back
--- electron-server/install_viewer.sh
+++ electron-server/install_viewer.sh
@@ -5,8 +5,9 @@ folder=electron-server/venv_viewer
 python3 -m venv $folder
 bin_folder=$folder/bin
 source $bin_folder/activate
+packages_path=$folder/lib/python3.10/site-packages
 pip install pip-tools
 pip-compile electron-server/requirements_viewer.in
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $bin_folder/geodeapp_viewer --distpath electron-server/dist_viewer
+pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $packages_path/geodeapp_viewer/app.py --distpath electron-server/dist_viewer -n geodeapp_viewer
