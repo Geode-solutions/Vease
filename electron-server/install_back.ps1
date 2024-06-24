5088639bbd79dcead172376a6389ae7b16c21eed
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-24 17:37:36 +0200

    fix(CICD): test ls electron-server

--- electron-server/install_back.ps1
+++ electron-server/install_back.ps1
@@ -13,4 +13,5 @@ pip-compile electron-server/requirements_back.in
 pip install -r electron-server/requirements_back.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_back --collect-data geodeapp_back $bin_folder/app.py --distpath ./electron-server/dist_back -n geodeapp_back --clean
+ls -r ./electron-server/dist_back
 Copy-Item ./electron-server/dist_back/geodeapp_back.exe ./resources
--- electron-server/install_viewer.ps1
+++ electron-server/install_viewer.ps1
@@ -13,4 +13,5 @@ pip-compile electron-server/requirements_viewer.in
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules electron-server/venv_viewer/Lib/site-packages/geodeapp_viewer/app.py --distpath ./electron-server/dist_viewer -n geodeapp_viewer --clean
+ls -r ./electron-server/dist_viewer
 Copy-Item ./electron-server/dist_viewer/geodeapp_viewer.exe ./resources
