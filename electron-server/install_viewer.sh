1edd3afaa8fff8dfb080c67f849f5548abfc9283
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-06-25 10:03:59 +0200

    fix(CICD): test fix scripts

--- electron-server/install_viewer.sh
+++ electron-server/install_viewer.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
-rm -rf electron-server/venv_viewer electron-server/dist_back electron-server/build electron-server/requirements_viewer.txt
+rm -rf electron-server/venv_viewer electron-server/dist_viewer electron-server/build electron-server/requirements_viewer.txt
 folder=electron-server/venv_viewer
 python3 -m venv $folder
 bin_folder=$folder/bin
@@ -11,5 +11,4 @@ pip-compile electron-server/requirements_viewer.in
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $packages_path/geodeapp_viewer/app.py --distpath ./electron-server/dist_viewer -n geodeapp_viewer --clean
-ls -r ./electron-server/dist_viewer
 cp ./electron-server/dist_viewer/geodeapp_viewer ./resources
