fc271d2f295c0deb6b72ba7cee94200c01936008
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-08-26 16:43:21 +0200

    fix(deps): requirements commited

--- electron-server/compile_back.ps1
+++ electron-server/compile_back.ps1
@@ -1,3 +1,3 @@
 Remove-Item -Recurse -Force /electron-server/requirements_back.txt -ErrorAction:SilentlyContinue
 pip install pip-tools
-pip-compile --pre electron-server/requirements_back.in
+pip-compile electron-server/requirements_back.in
--- electron-server/compile_back.sh
+++ electron-server/compile_back.sh
@@ -2,4 +2,4 @@
 set -e
 rm -rf electron-server/requirements_back.txt
 pip install pip-tools
-pip-compile --pre electron-server/requirements_back.in
+pip-compile electron-server/requirements_back.in
