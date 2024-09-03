5eb559bff6debe635954d9a6ddf319f2bd4ed631
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-09-03 16:04:38 +0200

    update scripts

--- electron-server/install_viewer.ps1
+++ electron-server/install_viewer.ps1
@@ -10,4 +10,4 @@ electron-server/venv_viewer/Scripts/Activate
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules ./electron-server/venv_viewer/Lib/site-packages/geodeapp_viewer/app.py --distpath ./electron-server/dist_viewer -n geodeapp_viewer --clean
-Copy-Item ./electron-server/dist_viewer/geodeapp_viewer.exe ./resources
+Copy-Item ./electron-server/dist_viewer/geodeapp_viewer.exe ./
