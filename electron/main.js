5774d36b3915814942a620980ec8984b3edf438d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-09-04 11:55:19 +0200

    PR comments

--- electron/main.js
+++ electron/main.js
@@ -126,7 +126,7 @@ app.whenReady().then(() => {
         "--port " + port,
         "--data_folder_path " + data_folder_path,
         "--desktop",
-        "-origin ",
+        "--allowed_origin ",
         "" * "",
       ],
       "Serving Flask app"
