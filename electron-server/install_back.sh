35b2d9d123a5d10d4b643f5f16c2b5b34825206a
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-10-29 16:31:03 +0100

    fix(app): change commands in all scripts update back & viewer deps

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -8,5 +8,5 @@ source $bin_folder/activate
 packages_path=$folder/lib/python3.10/site-packages
 pip install -r electron-server/requirements_back.txt
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back $packages_path/geodeapp_back/app.py --distpath ./electron-server/dist_back -n geodeapp_back --clean
-cp ./electron-server/dist_back/geodeapp_back ./
+pyinstaller --onefile --collect-data opengeodeweb_back $packages_path/vease_back/app.py --distpath ./electron-server/dist_back -n vease-back --clean
+cp ./electron-server/dist_back/vease-back ./
