b1d3d5138cf84e56563596d2244dc64b4cda4e52
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-11-06 16:03:07 +0100

    Add python 3.10 back

--- electron-server/install_back.sh
+++ electron-server/install_back.sh
@@ -5,7 +5,7 @@ folder=electron-server/venv_back
 python3 -m venv $folder
 bin_folder=$folder/bin
 source $bin_folder/activate
-packages_path=$folder/lib/python3.9/site-packages
+packages_path=$folder/lib/python3.10/site-packages
 pip install -r electron-server/requirements_back.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_back $packages_path/vease_back/app.py --distpath ./electron-server/dist_back -n vease-back --clean
--- electron-server/install_viewer.sh
+++ electron-server/install_viewer.sh
@@ -5,7 +5,7 @@ folder=electron-server/venv_viewer
 python3 -m venv $folder
 bin_folder=$folder/bin
 source $bin_folder/activate
-packages_path=$folder/lib/python3.9/site-packages
+packages_path=$folder/lib/python3.10/site-packages
 pip install -r electron-server/requirements_viewer.txt
 pip install pyinstaller
 pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $packages_path/vease_viewer/app.py --distpath ./electron-server/dist_viewer -n vease-viewer --clean
