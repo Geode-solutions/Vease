a32c76432bf44bbcb3ec60b79d7a21c2c8a396e4
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-11-07 11:29:22 +0100

    fix(script linux): working

--- electron-server/back/build.sh
+++ electron-server/back/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+back_path=./electron-server/back
+dist_path=$back_path/dist
+venv_path=$back_path/venv
+source $venv_path/bin/activate
+site_packages_path=$venv_path/lib/python3.10/site-packages
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back $packages_path/vease_back/app.py --distpath ./electron-server/back/dist -n vease-back --clean
-cp ./electron-server/back/dist/vease-back ./
+pyinstaller --onefile --collect-data opengeodeweb_back $site_packages_path/vease_back/app.py --distpath $dist_path -n vease-back --clean
+cp $dist_path/vease-back ./
---
+++ electron-server/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+microservice_name=$1
+echo "Compiling $microservice_name requirements"
+microservice_path="./electron-server/$microservice_name"
+venv_path=$microservice_path/venv
+python3 -m venv $venv_path
+rm -rf $microservice_path/requirements.txt
+source $venv_path/bin/activate
+pip install pip-tools
+pip-compile $microservice_path/requirements.in
