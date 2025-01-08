200fd3d6b7684c25090f84cd5be7cb87667a1e1b
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-08 14:33:06 +0100

    v-spacer sidebar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -2,68 +2,73 @@
   <v-navigation-drawer
     fixed
     v-model="drawer"
-    width="70"
+    width="80"
     align="center"
-    class="mt-10 ma-2"
     color="#FFFFFF00"
+    class="py-4"
     elevation="0"
     floating
     app
     permanent
   >
-    <!-- <v-row> -->
-      <v-tooltip v-for="(item, index) in items" :key="index" :text="item.title">
-        <template v-slot:activator="{ props }">
-          <v-btn
-            v-bind="props"
-            flat
-            color="#FFFFFF00"
-            @click="item.click"
-            style="border-radius: 20%"
-            :icon="item.icon"
-            class="mb-1"
-            draggable="true"
-            @dragstart="startDrag($event, item)"
-            @drop="onDrop($event, index)"
-            @dragover.prevent
-            data-type="sidebar-icon"
-          >
-            <v-icon
-              class="icon-style pa-6"
+    <v-row no-gutters class="flex-column" style="height:100%">
+      <v-col cols="auto"
+        v-for="(item, index) in items"
+        :key="index"
+      >
+        <v-tooltip :text="item.title">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              flat
+              color="#FFFFFF00"
+              @click="item.click"
+              style="border-radius: 20%"
               :icon="item.icon"
-              color="white"
-              size="28"
-            />
-          </v-btn>
-        </template>
-      </v-tooltip>
-    <!-- </v-row> -->
+              class="mb-1"
+              draggable="true"
+              @dragstart="startDrag($event, item)"
+              @drop="onDrop($event, index)"
+              @dragover.prevent
+              data-type="sidebar-icon"
+            >
+              <v-icon
+                class="icon-style pa-6"
+                :icon="item.icon"
+                color="white"
+                size="28"
+              />
+            </v-btn>
+          </template>
+        </v-tooltip>
+      </v-col>
 
-    <!-- <v-row> -->
       <v-spacer />
-      <v-tooltip text="Infos">
-        <template v-slot:activator="{ props }">
-          <v-btn
-            v-bind="props"
-            flat
-            color="#FFFFFF00"
-            @click="navigateTo('/infos')"
-            style="border-radius: 20%"
-            icon="mdi-information-variant-circle"
-            class="mb-1"
-            @dragover.prevent
-            data-type="sidebar-icon"
-          >
-            <v-icon
-              class="icon-style pa-6"
+      <v-col cols="auto">
+        <v-tooltip text="Infos">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              flat
+              color="#FFFFFF00"
+              @click="navigateTo('/infos')"
+              style="border-radius: 20%"
               icon="mdi-information-variant-circle"
-              color="white"
-              size="28"
-            />
-          </v-btn>
-        </template>
-      </v-tooltip>
-    <!-- </v-row> -->
+              class="mb-1"
+              @dragover.prevent
+              data-type="sidebar-icon"
+            >
+              <v-icon
+                class="icon-style pa-6"
+                icon="mdi-information-variant-circle"
+                color="white"
+                size="28"
+              />
+            </v-btn>
+          </template>
+        </v-tooltip>
+      </v-col>
+    </v-row>
 
     <NewProject :show_dialog="newproject" @close="newproject = false" />
     <OpenProject :show_dialog="openproject" @close="openproject = false" />
