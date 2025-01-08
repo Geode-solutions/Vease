f2ee6b0df4766906cc62d4f579de530dcd74d1da
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-08 14:32:50 +0100

    infos page

--- pages/infos.vue
+++ pages/infos.vue
@@ -1,78 +1,154 @@
 <template>
-  <v-row class="fill-height pa-5">
-    <v-col cols="12">
-      <v-card style="height: 100%; width: 100%; overflow-y: auto;" class="bg-primary">
-        <v-container>
-          <v-row class="pa-3">
-            <v-col cols="12" md="6">
-              <p class="text-h4 text-white">App version</p>
-              <v-divider />
-              <v-row class="pa-5">
-                <p class="text-white">The current version of the app is&nbsp</p>
-                <a
-                  :href="
-                    'https://github.com/Geode-solutions/Vease/releases/tag/v' +
-                    version
-                  "
-                  target="_blank"
-                  class="text-left text-white"
-                >
-                  {{ version }}</a
-                >
-              </v-row>
-            </v-col>
+  <v-card
+    style="height: 100%; width: 100%; overflow-y: auto; border-radius: 10px"
+    class="bg-primary"
+  >
+    <v-container>
+      <v-row class="pa-1">
+        <v-col cols="6" md="6">
+          <p class="text-h4 text-white">App version</p>
+          <v-divider />
+          <v-row class="pa-5">
+            <p class="text-white">The current version of the app is&nbsp</p>
+            <a
+              :href="
+                'https://github.com/Geode-solutions/Vease/releases/tag/v' +
+                version
+              "
+              target="_blank"
+              class="text-left text-white"
+            >
+              {{ version }}</a
+            >
           </v-row>
-          <v-row class="pa-3">
-            <v-col cols="12" md="6">
-              <p class="text-h4 text-white">Internal dependencies</p>
-              <v-divider />
-              <v-row class="pa-5">
-                <p class="text-white"> The app uses the following internal dependencies :</p>
-              </v-row>
+        </v-col>
+        <v-col cols="6" md="6">
+          <p class="text-h4 text-white">Microservices status</p>
+          <v-divider />
+          <v-row class="pa-5">
+            <v-table
+              class="pa-2"
+              density="compact"
+              style="background-color: transparent"
+              width="50%"
+            >
+              <tbody>
+                <tr>
+                  <td class="text-left text-white">Back</td>
+                  <td>
+                    <v-icon
+                      :icon="
+                        geode_store.is_running
+                          ? 'mdi-check-circle'
+                          : 'mdi-close-circle'
+                      "
+                      :color="geode_store.is_running ? 'white' : 'red'"
+                      v-tooltip:right="
+                        geode_store.is_running
+                          ? 'Microservice is running'
+                          : 'Microservice is not running'
+                      "
+                    />
+                  </td>
+                </tr>
 
-              <v-table
-                class="pa-2"
-                style="background-color: transparent"
-                width="50%"
-              >
-                <thead>
-                  <tr>
-                    <th class="text-left text-white" width="30%">Package</th>
-                    <th class="text-left text-white" width="50%">Version</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="pkg in packages_versions" :key="pkg">
-                    <td class="text-left text-white">{{ pkg.package }}</td>
-                    <td>
-                      <a
-                        :href="
-                          'https://pypi.org/project/' +
-                          pkg.package +
-                          '/' +
-                          pkg.version +
-                          '/'
-                        "
-                        target="_blank"
-                        class="text-left text-white"
-                        >{{ pkg.version }}</a
-                      >
-                    </td>
-                  </tr>
-                </tbody>
-              </v-table>
-            </v-col>
+                <tr>
+                  <td class="text-left text-white">Viewer</td>
+                  <td>
+                    <v-icon
+                      :icon="
+                        viewer_store.is_running
+                          ? 'mdi-check-circle'
+                          : 'mdi-close-circle'
+                      "
+                      :color="viewer_store.is_running ? 'white' : 'red'"
+                      v-tooltip:right="
+                        viewer_store.is_running
+                          ? 'Microservice is running'
+                          : 'Microservice is not running'
+                      "
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
           </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-col>
+      </v-row>
+
+      <v-row class="pa-1">
+        <v-col cols="12" md="6">
+          <p class="text-h4 text-white">Internal dependencies</p>
+          <v-divider />
+          <v-row class="pa-5">
+            <p class="text-white">
+              The app uses the following internal dependencies :
+            </p>
+          </v-row>
+
+          <v-table
+            class="pa-2"
+            density="compact"
+            style="background-color: transparent"
+            width="50%"
+          >
+            <thead>
+              <tr>
+                <th class="text-left text-white" width="50%">Package</th>
+                <th class="text-left text-white" width="10%">Version</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="pkg in packages_versions" :key="pkg">
+                <td class="text-left text-white">{{ pkg.package }}</td>
+                <td>
+                  <a
+                    :href="
+                      'https://pypi.org/project/' +
+                      pkg.package +
+                      '/' +
+                      pkg.version +
+                      '/'
+                    "
+                    target="_blank"
+                    class="text-left text-white"
+                    >{{ pkg.version }}</a
+                  >
+                </td>
+              </tr>
+            </tbody>
+          </v-table>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-btn
+            href="https://github.com/Geode-solutions/Vease/issues/new"
+            target="_blank"
+            >Report an issue</v-btn
+          >
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <p class="text-white">
+            Vease is an open-source project.
+            <br />
+            Copyright © 2019 - {{ new Date().getFullYear() }} — Geode-solutions
+            SAS. All rights reserved.
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script setup>
 import vease_back_schemas from "@geode/vease-back/schemas.json";
-console.log(vease_back_schemas);
+
 const version = useRuntimeConfig().public.VERSION;
+const geode_store = use_geode_store();
+const viewer_store = use_viewer_store();
 
 const packages_versions = ref([]);
 
