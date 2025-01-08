b0e2a56acadf7ed3e4c3dab26856238423a07d07
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-08 14:33:20 +0100

    row & col in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,12 @@
       @drop="onDrop"
       @mousemove="handleMouseMove"
     >
-      <NuxtPage style="z-index: 1" />
+      <v-row class="fill-height pa-2 mr-1">
+        <v-col cols="12" class="pa-1">
+          <NuxtPage style="z-index: 1" />
+        </v-col>
+      </v-row>
+
       <TopBar />
       <SideBar />
       <FeedBackSnackers />
