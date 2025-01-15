0b4dc2fe3aeac406ce5aaa49c627e2720dc46695
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-15 11:36:12 +0100

    fix(meta): explicitly import metadata

--- electron-server/back/build.sh
+++ electron-server/back/build.sh
@@ -5,5 +5,5 @@ venv_path=$back_path/venv
 source $venv_path/bin/activate
 site_packages_path=$venv_path/lib/python3.10/site-packages
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_back $site_packages_path/vease_back/app.py --distpath $dist_path -n vease-back --clean
+pyinstaller --onefile --collect-data opengeodeweb_back --collect-data vease_back --recursive-copy-metadata vease_back $site_packages_path/vease_back/app.py --distpath $dist_path -n vease-back --clean
 cp $dist_path/vease-back ./
--- electron-server/viewer/build.sh
+++ electron-server/viewer/build.sh
@@ -5,5 +5,5 @@ venv_path=$viewer_path/venv
 source $venv_path/bin/activate
 site_packages_path=$venv_path/lib/python3.10/site-packages
 pip install pyinstaller
-pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-all vtkmodules $site_packages_path/vease_viewer/app.py --distpath $dist_path -n vease-viewer --clean
+pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-data vease_viewer --collect-all vtkmodules $site_packages_path/vease_viewer/app.py --distpath $dist_path -n vease-viewer --clean
 cp $dist_path/vease-viewer ./
