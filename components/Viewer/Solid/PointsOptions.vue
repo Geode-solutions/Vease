24180f45c9f59a72ceaa631d7a76dfa51414f8dd
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-01-23 15:47:55 +0100

    wip

---
+++ components/Viewer/Solid/PointsOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <ViewerGenericMeshPointsOptions
+    :itemProps="props.itemProps"
+    :btn_image="SurfacePoints"
+  />
+</template>
+
+<script setup>
+// TO CHANGE
+import SurfacePoints from "@/assets/viewer_svgs/surface_points.svg";
+
+const props = defineProps({
+  itemProps: { type: Object, required: true },
+});
+</script>
---
+++ components/Viewer/Solid/PolyhedronsOptions.vue
@@ -0,0 +1,15 @@
+<template>
+  <ViewerGenericMeshPolyhedronsOptions
+    :itemProps="props.itemProps"
+    :btn_image="Solid2DTriangles"
+    tooltip="Triangles options"
+  />
+</template>
+
+<script setup>
+import Solid2DTriangles from "@/assets/viewer_svgs/surface_triangles.svg";
+
+const props = defineProps({
+  itemProps: { type: Object, required: true },
+});
+</script>
