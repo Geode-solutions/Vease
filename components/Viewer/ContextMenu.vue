d7917dd2eb7ec057ba97698ba67b7d02156790b5
SpliiT <spliitdev@gmail.com>
2025-02-21 13:51:57 +0100

    rv logs

--- components/Viewer/ContextMenu.vue
+++ components/Viewer/ContextMenu.vue
@@ -42,10 +42,7 @@ const show_menu = ref(true);
 
 watch(show_menu, (value) => {
   if (!value) {
-    console.log("Context menu closed");
     menuStore.closeMenu();
-  } else {
-    console.log("Context menu opened");
   }
 });
 
