68c8760174ac1932763738c64a7ca914e50dc367
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-03-14 10:19:03 +0100

    cleanup

--- stores/data_style.js
+++ stores/data_style.js
@@ -3,9 +3,6 @@ import useMeshStyle from "../internal_stores/mesh/index.js";
 
 export const useDataStyleStore = defineStore("dataStyle", () => {
   return {
-    // objectVisibility,
-    // selectedObjects,
-    // addDataStyle,
     ...useState(),
     ...useMeshStyle(),
   };
