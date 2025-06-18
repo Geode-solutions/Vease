f2e3a64db2d83b18df5e769b6f9184b7c3ccac55
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-06-18 13:11:39 +0200

    test config

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    name: "e2e",
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
