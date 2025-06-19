b54a4894faebabf6f6dafb5d47ac9e733c9a62b2
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-06-19 15:45:10 +0200

    save wip

--- e2e/electron.test.js
+++ e2e/electron.test.js
@@ -1,27 +1,21 @@
-const { _electron: electron } = require("playwright");
+const { expect, test, _electron: electron } = require("@playwright/test");
 
-(async () => {
-  // Launch Electron app.
-  const electronApp = await electron.launch();
-
-  // Evaluation expression in the Electron context.
-  const appPath = await electronApp.evaluate(async ({ app }) => {
-    // This runs in the main Electron process, parameter here is always
+test("save screenshot", async () => {
+  const electronApp = await electron.launch({ args: ["."] });
+  console.log("App launched!");
+  const isPackaged = await electronApp.evaluate(async ({ app }) => {
+    // This runs in Electron's main process, parameter here is always
     // the result of the require('electron') in the main app script.
-    return app.getAppPath();
+    return app.isPackaged;
   });
-  console.log(appPath);
 
-  // Get the first window that the app opens, wait if necessary.
+  expect(isPackaged).toBe(true);
+
+  // Wait for the first BrowserWindow to open
+  // and return its Page object
   const window = await electronApp.firstWindow();
-  // Print the title.
-  console.log(await window.title());
-  // Capture a screenshot.
   await window.screenshot({ path: "intro.png" });
-  // Direct Electron console to Node terminal.
-  window.on("console", console.log);
-  // Click button.
 
-  // Exit app.
+  // close app
   await electronApp.close();
-})();
+});
