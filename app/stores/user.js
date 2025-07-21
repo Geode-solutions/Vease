024ef08a63477b8c569d913d896d5fee1d844b36
MaxNumerique <maximilienlandois@gmail.com>
2025-07-21 12:03:52 +0200

    user store

--- app/stores/user.js
+++ app/stores/user.js
@@ -1,13 +1,25 @@
-export const useUserStore = defineStore("user", {
-  state: () => ({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    image: "",
-  }),
-  actions: {
-    fetchUserData() {},
-    updateProfile() {},
-  },
-});
+export const useUserStore = defineStore('user', () => {
+  const firstName = ref('')
+  const lastName = ref('')
+  const email = ref('')
+  const password = ref('')
+  const image = ref('')
+
+  const fetchUserData = async () => {
+    // code pour récupérer les données de l'utilisateur
+  }
+
+  const updateProfile = async () => {
+    // code pour mettre à jour le profil de l'utilisateur
+  }
+
+  return { 
+    firstName,
+    lastName, 
+    email, 
+    password, 
+    image, 
+    fetchUserData, 
+    updateProfile 
+  }
+})
