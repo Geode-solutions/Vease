f6a299c088a1b20c5ac5c8156da2720be6a4dc18
MaxNumerique <maximilienlandois@gmail.com>
2025-07-22 14:15:48 +0200

    fix(schemas): update imports, dependencies for ogweb-back and ogweb-front

--- internal_stores/mesh/points.js
+++ internal_stores/mesh/points.js
@@ -1,4 +1,4 @@
-import viewer_schemas from "@geode/opengeodeweb-viewer/schemas.json";
+import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json";
 const mesh_points_schemas = viewer_schemas.opengeodeweb_viewer.mesh.points;
 
 export function useMeshPointsStyle() {
--- internal_stores/mesh/polygons.js
+++ internal_stores/mesh/polygons.js
@@ -1,4 +1,4 @@
-import viewer_schemas from "@geode/opengeodeweb-viewer/schemas.json";
+import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json";
 const mesh_polygons_schemas = viewer_schemas.opengeodeweb_viewer.mesh.polygons;
 
 export function useMeshPolygonsStyle() {
