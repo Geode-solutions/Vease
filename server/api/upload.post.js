64c51391087a7b789d0949f433e4b946f1c2e2c6
MaxNumerique <maximilienlandois@gmail.com>
2025-07-24 23:09:56 +0200

    test

--- server/api/upload.post.js
+++ server/api/upload.post.js
@@ -2,26 +2,12 @@ import { readFiles } from "h3-formidable";
 import { errors as formidableErrors } from "formidable";
 import path from "path";
 import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
-import os from "os";
 
 export default defineEventHandler(async (event) => {
   const maxFiles = 1;
   const fileSize = 1024 * 1024 * 5; // 5MB
 
   try {
-    const projectUuid = uuidv4();
-    
-    const projectPath = path.join(os.tmpdir(), "vease", projectUuid);
-    const uploadPath = path.join(projectPath, "uploads");
-    
-    if (!fs.existsSync(projectPath)) {
-      fs.mkdirSync(projectPath, { recursive: true });
-    }
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-
     const { files } = await readFiles(event, {
       maxFiles: maxFiles,
       maxFileSize: fileSize,
@@ -33,8 +19,6 @@ export default defineEventHandler(async (event) => {
         statusCode: 400,
       }); 
     }
-    
-    const uploadedFiles = [];
     
     for (let index = 0; index < Object.keys(files).length; index++) {
       const filepath = files[index][0].filepath;
@@ -48,22 +32,14 @@ export default defineEventHandler(async (event) => {
       }
       
       let imageName = `${String(Date.now()) + String(Math.round(Math.random() * 10000000))}.${mimetype.split("/")[1]}`;
-      let newPath = path.join(uploadPath, imageName);
+      let newPath = path.join(upload, imageName);
       fs.copyFileSync(filepath, newPath);
-      
-      uploadedFiles.push({
-        filename: imageName,
-        path: newPath,
-        projectUuid: projectUuid
-      });
     }
 
     return {
       status: 200,
       message: "Upload image successfully.",
-      projectUuid: projectUuid,
-      files: uploadedFiles
-    };
+    }
   } catch (error) {
     if (error.message === "2001") {
       throw createError({
