2ee8514d969edd84f30a771c1305975761fd492a
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-08-01 14:24:27 +0200

    separate browser & desktop tests [skip ci]

---
+++ tests/e2e/browser/playwright.config.js
@@ -0,0 +1,60 @@
+// @ts-check
+import { fileURLToPath } from "node:url";
+import { defineConfig, devices } from "@playwright/test";
+import path from "path";
+
+const __dirname = path.resolve();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+    expect: {
+        toHaveScreenshot: { maxDiffPixelRatio: 0.02 },
+    },
+    testDir: "./",
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'http://localhost:3000',
+        nuxt: {
+            rootDir: fileURLToPath(new URL(".", import.meta.url)),
+            dev: true,
+        },
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: "on-first-retry",
+    },
+
+    projects: [
+        {
+            name: "chromium",
+            use: {
+                ...devices["Desktop Chrome"],
+            },
+        },
+        // {
+        //   name: "firefox",
+        //   use: {
+        //     ...devices["Desktop Firefox"],
+        //   },
+        // },
+    ],
+
+    webServer: {
+        name: "Front",
+        command: "NODE_ENV=test npx nuxt preview ../../..",
+        port: 3000,
+        reuseExistingServer: true,
+    },
+});
