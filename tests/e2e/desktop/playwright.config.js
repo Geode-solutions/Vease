5c26901cd45d1a9e980d574f3fea5b8fcc637364
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-08-04 14:48:21 +0200

    testDir

--- tests/e2e/desktop/playwright.config.js
+++ tests/e2e/desktop/playwright.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   expect: {
     toHaveScreenshot: { maxDiffPixelRatio: 0.02 },
   },
-  testDir: "./tests/e2e",
+  testDir: "./tests/e2e/desktop",
   timeout: (isWindows ? 60 : 30) * 1000,
   /* Run tests in files in parallel */
   fullyParallel: true,
