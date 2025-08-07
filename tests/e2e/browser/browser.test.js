e6a25cbd6559d41c957a88a0f991d7e6e843fc4f
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-08-07 17:22:00 +0200

    test

--- tests/e2e/browser/browser.test.js
+++ tests/e2e/browser/browser.test.js
@@ -10,7 +10,7 @@ test("Window title", async ({ page }) => {
 test("Microservices running", async ({ page }) => {
   await page.goto("/");
   await page.setViewportSize({ width: 1200, height: 800 });
-  await page.waitForTimeout((isWindows ? 20 : 10) * 1000);
+  await page.waitForTimeout((isWindows ? 30 : 10) * 1000);
   await expect(page).toHaveScreenshot({
     path: `microservices-running-${process.platform}.png`,
   });
