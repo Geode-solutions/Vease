ab61ecb660a7874ceecadf5beba50490db48957e
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-08-08 11:04:06 +0200

    reuseExistingServer: false

--- tests/e2e/browser/playwright.config.mjs
+++ tests/e2e/browser/playwright.config.mjs
@@ -54,7 +54,7 @@ export default defineConfig({
     name: "Front",
     command: "npm run test:browser",
     port: 3000,
-    reuseExistingServer: true,
+    reuseExistingServer: false,
     stdout: "pipe",
     stderr: "pipe",
   },
