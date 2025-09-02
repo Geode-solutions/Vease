bb74815759602d10a39d816361ca77bf4956e070
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-09-02 10:25:05 +0200

    comment firefox tests

--- tests/e2e/browser/playwright.config.js
+++ tests/e2e/browser/playwright.config.js
@@ -36,17 +36,17 @@ export default defineConfig({
   },
 
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: {
-    //     ...devices["Desktop Chrome"],
-    //   },
-    // },
     {
-      name: "firefox",
+      name: "chromium",
       use: {
-        ...devices["Desktop Firefox"],
+        ...devices["Desktop Chrome"],
       },
     },
+    // {
+    //   name: "firefox",
+    //   use: {
+    //     ...devices["Desktop Firefox"],
+    //   },
+    // },
   ],
 });
