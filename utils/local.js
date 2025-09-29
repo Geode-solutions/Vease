91e7f9aca8b357e283f85244c7d4af0c5a792b82
BotellaA <3213882+BotellaA@users.noreply.github.com>
2025-09-29 18:25:54 +0000

    Apply prepare changes

--- utils/local.js
+++ utils/local.js
@@ -67,7 +67,7 @@ async function run_script(
   command,
   args,
   expected_response,
-  timeout_seconds = 30
+  timeout_seconds = 30,
 ) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -117,7 +117,7 @@ async function run_back(port, project_folder_path) {
   return new Promise(async (resolve, reject) => {
     const back_command = path.join(
       executable_path(path.join("microservices", "back")),
-      executable_name("vease-back")
+      executable_name("vease-back"),
     )
     const back_port = await get_available_port(port)
     const back_args = [
@@ -136,7 +136,7 @@ async function run_viewer(port, data_folder_path) {
   return new Promise(async (resolve, reject) => {
     const viewer_command = path.join(
       executable_path(path.join("microservices", "viewer")),
-      executable_name("vease-viewer")
+      executable_name("vease-viewer"),
     )
     const viewer_port = await get_available_port(port)
     const viewer_args = [
@@ -195,7 +195,7 @@ async function run_browser(script_name) {
     nuxt_process.stdout.on("data", function (data) {
       const output = data.toString()
       const portMatch = output.match(
-        /Accepting\ connections\ at\ http:\/\/localhost:(\d+)/
+        /Accepting\ connections\ at\ http:\/\/localhost:(\d+)/,
       )
       if (portMatch) {
         resolve(portMatch[1])
