4ea427e5747d88f3c007fb865500f853ff78b4ad
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-10-03 11:47:05 +0200

    fix paths & imports

--- node_scripts/browser.js
+++ node_scripts/browser.js
@@ -1,24 +1,25 @@
 // Standard library imports
 import os from "os"
+import path from "path"
 // Third party imports
 
 // Local imports
 import {
+  create_path,
   executable_name,
   executable_path,
   run_browser,
-} from "@ogw_f/utils/local.js"
+} from "@geode/opengeodeweb-front/utils/local.js"
 
 const data_folder_path = create_path(path.join(os.tmpdir(), "vease"))
-
 const back_command = path.join(
   executable_path(path.join("microservices", "back")),
-  executable_name("vease-back"),
+  executable_name("vease-back")
 )
 
 const viewer_command = path.join(
   executable_path(path.join("microservices", "viewer")),
-  executable_name("vease-viewer"),
+  executable_name("vease-viewer")
 )
 await run_browser(process.argv[2], {
   back: { command: back_command, args: { port: 5000, data_folder_path } },
