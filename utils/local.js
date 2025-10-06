7017ae232bdd674dd9dc5259d37314cda3e88826
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-10-06 13:22:40 +0200

    test

--- utils/local.js
+++ utils/local.js
@@ -13,14 +13,17 @@ import {
 
 function run_browser_wrapper(script_name) {
   const data_folder_path = create_path(path.join(os.tmpdir(), "vease"))
+  console.log("data_folder_path", data_folder_path)
   const back_command = path.join(
     executable_path(path.join("microservices", "back")),
     executable_name("vease-back")
   )
+  console.log("back_command", back_command)
   const viewer_command = path.join(
     executable_path(path.join("microservices", "viewer")),
     executable_name("vease-viewer")
   )
+  console.log("viewer_command", viewer_command)
   return new Promise(async (resolve, reject) => {
     const NUXT_PORT = await run_browser(script_name, {
       back: { command: back_command, args: { port: 5000, data_folder_path } },
