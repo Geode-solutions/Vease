487b85689c009f60ae50c2f3b81a281b9a720af4
MaxNumerique <maximilienlandois@gmail.com>
2025-10-17 10:33:59 +0200

    removed plugin

--- app/plugins/autoRegister.js
+++
@@ -1,6 +0,0 @@
-import autoStoreRegister from "@geode/opengeodeweb-front/plugins/autoStoreRegister"
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.$pinia.use(autoStoreRegister)
-  console.log("[AUTOREGISTER PLUGIN] Loaded")
-})
