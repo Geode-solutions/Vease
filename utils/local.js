f955dcc14e8a29fc07488822a3a30625918d5c13
Arnaud Botella <arnaud.botella@geode-solutions.com>
2025-11-05 10:25:13 +0100

    fix(Local): allow use of executable in path

--- utils/local.js
+++ utils/local.js
@@ -12,25 +12,36 @@ import {
   run_browser,
 } from "@geode/opengeodeweb-front/utils/local.js"
 
+function back_microservice() {
+  const back_path = executable_path(path.join("microservices", "back"))
+  const back_name = executable_name("vease-back")
+  return { back_name, back_path }
+}
+
+function viewer_microservice() {
+  const viewer_path = executable_path(path.join("microservices", "viewer"))
+  const viewer_name = executable_name("vease-viewer")
+  return { viewer_name, viewer_path }
+}
+
 function run_browser_wrapper(script_name) {
   const project_folder_path = create_path(
     path.join(os.tmpdir(), "vease", uuidv4()),
   )
   console.log("project_folder_path", project_folder_path)
-  const back_command = path.join(
-    executable_path(path.join("microservices", "back")),
-    executable_name("vease-back"),
-  )
-  console.log("back_command", back_command)
-  const viewer_command = path.join(
-    executable_path(path.join("microservices", "viewer")),
-    executable_name("vease-viewer"),
-  )
-  console.log("viewer_command", viewer_command)
+  const { back_name, back_path } = back_microservice()
+  console.log("back_command", back_path, back_name)
+  const { viewer_name, viewer_path } = viewer_microservice()
+  console.log("viewer_command", viewer_path, viewer_name)
   return run_browser(script_name, {
-    back: { command: back_command, args: { project_folder_path } },
+    back: {
+      executable_name: back_name,
+      executable_path: back_path,
+      args: { project_folder_path },
+    },
     viewer: {
-      command: viewer_command,
+      executable_name: viewer_name,
+      executable_path: viewer_path,
       args: { project_folder_path },
     },
   })
