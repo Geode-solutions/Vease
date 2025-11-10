0c3b355082bfebe4bd5c45650acb5a85eb4f47a9
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-11-10 10:13:14 +0100

    fix(Recaptcha): request params body

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,6 +1,8 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/utils/recaptcha.js"
 
 exports.handler = async function (event) {
-  const { name, email, launch } = event.queryStringParameters
+  console.log("handler", { event })
+  const { name, email, launch } = event.body
+
   return check_recaptcha_params(name, email, launch)
 }
