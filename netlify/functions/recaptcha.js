0c93f867f7c239a02dcf61f7c7897dc2e09c5707
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-11-10 15:32:54 +0100

    pass event as param

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,7 +1,6 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/utils/recaptcha.js"
 
-exports.handler = function (event) {
+exports.handler = async function (event) {
   console.log("handler", { event })
-  const { name, email, launch } = event.body
-  return check_recaptcha_params(name, email, launch)
+  return check_recaptcha_params(event)
 }
