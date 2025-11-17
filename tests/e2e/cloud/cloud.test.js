d81a24ddd98b14aeeae424863eb31c3408903b3d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-11-17 09:49:05 +0100

    uncomment

--- tests/e2e/cloud/cloud.test.js
+++ tests/e2e/cloud/cloud.test.js
@@ -4,13 +4,12 @@ test.beforeEach(async ({ page }) => {
   page.on("console", (msg) => console.log(`Browser console: ${msg.text()}`))
 
   let prefix = ""
-  // if (process.env.GITHUB_REF_NAME === "next") {
-  //   prefix = "next."
-  // }
-  // await page.goto(`https://${prefix}vease.geode-solutions.com`)
-  await page.goto(`http://localhost:8888/`)
+  if (process.env.GITHUB_REF_NAME === "next") {
+    prefix = "next."
+  }
+  await page.goto(`https://${prefix}vease.geode-solutions.com`)
   await page.getByRole("button", { name: "Launch the app" }).click()
-  // await page.waitForTimeout(100 * 1000)
+  await page.waitForTimeout(100 * 1000)
   console.log("Navigated to", page.url())
   await page.setViewportSize({ width: 1200, height: 800 })
 })
