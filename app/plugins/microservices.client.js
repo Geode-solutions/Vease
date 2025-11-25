95d056ab5fde47ec6156db71a43b638875d2515a
MaxNumerique <maximilienlandois@gmail.com>
2025-11-25 15:25:28 +0100

    register_microservice method instead of init_microservice without dups

--- app/plugins/microservices.client.js
+++ app/plugins/microservices.client.js
@@ -1,4 +1,4 @@
-import { registerMicroservice } from "@ogw_front/stores/infra.js"
+import { useInfraStore } from "@ogw_front/stores/infra.js"
 import { useGeodeStore } from "@ogw_front/stores/geode.js"
 import { useViewerStore } from "@ogw_front/stores/viewer.js"
 
@@ -6,14 +6,14 @@ export default defineNuxtPlugin({
   name: "microservices",
   hooks: {
     "app:beforeMount"() {
-      registerMicroservice({
+      const infra_store = useInfraStore()
+      infra_store.register_microservice({
         name: "geode",
         useStore: useGeodeStore,
         connect: (store) => store.do_ping(),
         electron_runner: "run_back",
       })
-
-      registerMicroservice({
+      infra_store.register_microservice({
         name: "viewer",
         useStore: useViewerStore,
         connect: (store) => store.ws_connect(),
