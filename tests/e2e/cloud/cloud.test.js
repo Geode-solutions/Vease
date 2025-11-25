c63760d5776a6ce3e8d913749034ed66a4a832bb
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-11-25 11:19:03 +0100

    fix(TestsE2eCloud): run git to get branch name

--- tests/e2e/cloud/cloud.test.js
+++ tests/e2e/cloud/cloud.test.js
@@ -1,10 +1,13 @@
 import { expect, test } from "@playwright/test"
+import { execSync } from 'child_process';
 
 test.beforeEach(async ({ page }) => {
   page.on("console", (msg) => console.log(`Browser console: ${msg.text()}`))
 
   let prefix = ""
-  if (process.env.GITHUB_REF_NAME === "next") {
+  const branch = execSync('git branch --show-current', { encoding: 'utf8' }).trim();
+  console.log("Current branch:", branch)
+  if (branch === "next") {
     prefix = "next."
   }
   await page.goto(`https://${prefix}vease.geode-solutions.com`)
