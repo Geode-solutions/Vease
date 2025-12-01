5313dc942163e7775277dc5b961ff79520de426b
SpliiT <spliitdev@gmail.com>
2025-12-01 15:59:02 +0100

    clear the files

--- app/plugins/extension.client.js
+++ app/plugins/extension.client.js
@@ -1,7 +1,6 @@
 import * as Vue from 'vue'
 import * as Pinia from 'pinia'
 import { VeaseExtensionAPI } from '../utils/extensionAPI.js'
-import back_schemas from "@geode/opengeodeweb-back/opengeodeweb_back_schemas.json"
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (typeof window !== 'undefined') {
@@ -10,16 +9,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   }
   
   const extensionsStore = useExtensionsStore()
-  const uiStore = useUIStore()
-  const hybridViewerStore = useHybridViewerStore()
-  const dataBaseStore = useDataBaseStore()
   
-  const extensionAPI = new VeaseExtensionAPI({ 
-    schemas: back_schemas,
-    uiStore,
-    hybridViewerStore,
-    dataBaseStore
-  })
+  const extensionAPI = new VeaseExtensionAPI()
   
   extensionsStore.setExtensionAPI(extensionAPI)
   nuxtApp.vueApp.provide('extensionAPI', extensionAPI)
--- app/utils/extensionAPI.js
+++ app/utils/extensionAPI.js
@@ -1,27 +1,6 @@
 export class VeaseExtensionAPI {
-  constructor({ schemas, uiStore, hybridViewerStore, dataBaseStore } = {}) {
-    this._uiStore = uiStore
-    this._hybridViewerStore = hybridViewerStore
-    this._dataBaseStore = dataBaseStore
-    this._importItem = null
-    this._schemas = schemas || null
+  constructor() {
     this.currentExtensionId = null
-  }
-
-  get UIStore() {
-    return this._uiStore
-  }
-
-  get HybridViewerStore() {
-    return this._hybridViewerStore
-  }
-
-  get DataBaseStore() {
-    return this._dataBaseStore
-  }
-
-  getSchemas() {
-    return this._schemas
   }
 
   setCurrentExtensionId(id) {
@@ -40,39 +19,14 @@ export class VeaseExtensionAPI {
       throw new Error('Tool definition must have a component')
     }
 
-    this.UIStore.registerToolComponent(toolDefinition, this.currentExtensionId)
+    useUIStore().registerToolComponent(toolDefinition, this.currentExtensionId)
   }
 
   unregisterTool(toolId) {
-    this.UIStore.unregisterTool(toolId)
+    useUIStore().unregisterTool(toolId)
   }
 
   unregisterToolsByExtension(extensionId) {
-    this.UIStore.unregisterToolsByExtension(extensionId)
-  }
-
-  get vue() {
-    return {
-      defineAsyncComponent,
-      markRaw,
-      ref,
-      computed,
-      watch,
-      onMounted,
-      onUnmounted,
-      inject,
-    }
-  }
-
-  get api_fetch() {
-    return api_fetch
-  }
-
-  async importItem(data) {
-    if (!this._importItem) {
-      const module = await import('@ogw_front/utils/file_import_workflow.js')
-      this._importItem = module.importItem
-    }
-    return this._importItem(data)
+    useUIStore().unregisterToolsByExtension(extensionId)
   }
 }
