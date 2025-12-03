805677eaa70751ba44988a79c9b7d335691bd40d
MaxNumerique <maximilienlandois@gmail.com>
2025-12-03 11:38:44 +0100

    remove plugin in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,6 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
   ].filter(Boolean),
 
-  plugins: ["@geode/opengeodeweb-front/app/plugins/auto_store_register.js"],
-
   ssr: false,
   electron: {
     build: [
