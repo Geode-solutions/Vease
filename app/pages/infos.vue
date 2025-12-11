189193bc549f86ad92993206c8a07802449cfadb
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-12-11 14:12:45 +0100

    same

--- app/pages/infos.vue
+++ app/pages/infos.vue
@@ -163,7 +163,7 @@
   import vease_back_schemas from "@geode/vease-back/vease_back_schemas.json"
   import vease_viewer_schemas from "@geode/vease-viewer/vease_viewer_schemas.json"
   import Status from "@ogw_front/utils/status"
-  import { run_function_when_microservices_connected } from "@ogw_front/composables/run_function_when_microservices_connected
+  import { run_function_when_microservices_connected } from "@ogw_front/composables/run_function_when_microservices_connected"
   import { useGeodeStore } from "@ogw_front/stores/geode"
   import { useViewerStore } from "@ogw_front/stores/viewer"
 
