341b7075c8b993cc6624d930928148281db67f7f
SpliiT <spliitdev@gmail.com>
2026-01-07 12:11:38 +0100

    edit extensionAPI

--- app/api/extensionAPI.js
+++ app/api/extensionAPI.js
@@ -1,22 +1,9 @@
-import { useUIStore } from "../stores/UI"
-import { useDataStore } from "@ogw_front/stores/data"
-import { useHybridViewerStore } from "@ogw_front/stores/hybrid_viewer"
-import { useTreeviewStore } from "@ogw_front/stores/treeview"
-import { useDataStyleStore } from "@ogw_front/stores/data_style"
+import { useUIStore } from "@vease/stores/UI"
 
 export const useExtensionAPI = () => {
   const UIStore = useUIStore()
-  const dataStore = useDataStore()
-  const hybridViewerStore = useHybridViewerStore()
-  const treeviewStore = useTreeviewStore()
-  const dataStyleStore = useDataStyleStore()
 
   return {
-    UIStore,
-    dataStore,
-    hybridViewerStore,
-    treeviewStore,
-    dataStyleStore,
     registerDataManagerTab: UIStore.registerDataManagerTab,
   }
 }
