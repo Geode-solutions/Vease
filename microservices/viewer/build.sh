dc1965f590db39f699e4022cb6cf1229354046ac
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-01-12 11:52:01 +0100

    test

--- microservices/viewer/build.sh
+++ microservices/viewer/build.sh
@@ -5,6 +5,5 @@ venv_path=$viewer_path/venv
 source $venv_path/bin/activate
 site_packages_path=$venv_path/lib/python3.12/site-packages
 pip install pyinstaller
-ls /usr/lib/x86_64-linux-gnu/
-pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-data vease_viewer --collect-all vtkmodules $site_packages_path/vease_viewer/app.py --distpath $dist_path -n vease-viewer --clean --add-binary="/usr/lib/x86_64-linux-gnu/libGL*:." --add-binary="/usr/lib/x86_64-linux-gnu/dri:dri"  --add-binary="/usr/lib/x86_64-linux-gnu/libstdc++.*:." --add-binary="/usr/lib/x86_64-linux-gnu/libc.*.:." --add-binary="/usr/lib/x86_64-linux-gnu/libm.*.:." --runtime-hook=$viewer_path/hook.py
+pyinstaller --onefile --collect-data opengeodeweb_viewer --collect-data vease_viewer --collect-all vtkmodules $site_packages_path/vease_viewer/app.py --distpath $dist_path -n vease-viewer --clean --add-binary="/usr/lib/x86_64-linux-gnu/libGL*.so*:." --add-binary="/usr/lib/x86_64-linux-gnu/dri:dri"  --add-binary="/usr/lib/x86_64-linux-gnu/libstdc++.so*:." --add-binary="/usr/lib/x86_64-linux-gnu/libc.so*:." --add-binary="/usr/lib/x86_64-linux-gnu/libm.so*:." --runtime-hook=$viewer_path/hook.py
 cp $dist_path/vease-viewer ./
