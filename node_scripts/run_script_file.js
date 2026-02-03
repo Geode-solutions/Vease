5dda265ac29f05198e36d4dbbded78226fd4e752
MaxNumerique <maximilienlandois@gmail.com>
2026-02-03 14:17:25 +0100

    recaptcha, customSign, run_script_file, local

--- node_scripts/run_script_file.js
+++ node_scripts/run_script_file.js
@@ -1,40 +1,41 @@
-import { exec } from "child_process"
+import { exec } from "node:child_process"
+import { promisify } from "node:util"
 
-function execRun(cmd) {
-  return new Promise((resolve, reject) => {
-    const child = exec(cmd, (error, stdout) => {
-      if (error) {
-        if (error.code === 1) {
-          resolve(stdout)
-        } else {
-          reject(error)
-        }
-      } else {
-        resolve(stdout)
-      }
-    })
+const execAsync = promisify(exec)
 
-    child.stdout.on("data", function (data) {
-      console.log(data)
-    })
-    child.stderr.on("data", function (data) {
-      console.log(data)
-    })
-    child.on("exit", function () {
-      console.log("Exit script")
-    })
-    child.stdin.end()
+async function execRun(cmd) {
+  const promise = execAsync(cmd)
+  const { child } = promise
+
+  child.stdout.on("data", (data) => {
+    console.log(data)
   })
+  child.stderr.on("data", (data) => {
+    console.log(data)
+  })
+  child.on("exit", () => {
+    console.log("Exit script")
+  })
+  child.stdin.end()
+
+  try {
+    const { stdout } = await promise
+    return stdout
+  } catch (error) {
+    if (error.code === 1) {
+      return error.stdout
+    }
+    throw error
+  }
 }
 
 console.log("process.argv", process.argv)
 
-var script_name = process.argv[2]
-var command
-if (process.platform === "win32") {
-  command =
-    `powershell.exe ./microservices/${script_name}.ps1 ` + process.argv[3]
-} else if (process.platform === "linux") {
-  command = `bash ./microservices/${script_name}.sh ` + process.argv[3]
-}
+const args = process.argv.slice(2)
+const [script_name, last_arg] = args
+const command =
+  process.platform === "win32"
+    ? `powershell.exe ./microservices/${script_name}.ps1 ${last_arg}`
+    : `bash ./microservices/${script_name}.sh ${last_arg}`
+
 execRun(command)
