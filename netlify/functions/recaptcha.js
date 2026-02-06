c072a2bb6fa54a4613f2ec0b33bfb13258488140
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-02-06 15:07:53 +0100

    fix(RecaptchaParams): function taxes explicit params

--- netlify/functions/recaptcha.js
+++ netlify/functions/recaptcha.js
@@ -1,6 +1,6 @@
 import { check_recaptcha_params } from "@geode/opengeodeweb-front/app/utils/recaptcha.js"
 
 exports.handler = async function (event) {
-  console.log("handler", { event })
-  return check_recaptcha_params(event)
+  const { name, email, launch } = JSON.parse(event.body)
+  return check_recaptcha_params(name, email, launch)
 }
