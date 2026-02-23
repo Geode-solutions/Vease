3242c8d45962125f41e2ad09670b6f0956a3c371
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-02-23 08:16:21 +0100

    test

--- microservices/viewer/build.ps1
+++ microservices/viewer/build.ps1
@@ -3,5 +3,5 @@ $viewer_path=".\microservices\viewer"
 $dist_path="$viewer_path\dist"
 . "$viewer_path\venv\Scripts\activate"
 pip install pyinstaller
-pyinstaller $viewer_path\vease_viewer.spec
+pyinstaller $viewer_path\vease-viewer.spec
 Copy-Item $dist_path\vease-viewer.exe .\
