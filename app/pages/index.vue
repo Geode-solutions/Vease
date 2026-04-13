dff4179d335cd3491415ac95dfc54982f50294f6
MaxNumerique <maximilienlandois@gmail.com>
2026-04-13 14:54:08 +0200

    feat(modelStyles): add logic to color all model_component by type

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,23 +23,32 @@ const viewerUI = useTemplateRef("viewerUI");
 
 const { display_menu } = storeToRefs(menuStore);
 
-async function handleTreeMenu({ event, itemId, context_type, modelId }) {
+async function handleTreeMenu({ event, itemId, context_type, modelId, modelComponentType }) {
   const rect = cardContainer.value.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const yUI = event.clientY - rect.top;
 
-  const meta_data =
-    context_type === "model_component"
-      ? {
-          viewer_type: "model_component",
-          geode_object_type: "component",
-          modelId,
-          pickedComponentId: itemId,
-        }
-      : await dataStore.item(itemId);
+  let meta_data;
+  if (context_type === "model_component") {
+    meta_data = {
+      viewer_type: "model_component",
+      geode_object_type: "component",
+      modelId,
+      pickedComponentId: itemId,
+    };
+  } else if (context_type === "model_component_type") {
+    meta_data = {
+      viewer_type: "model_component_type",
+      geode_object_type: "type",
+      modelId,
+      modelComponentType: modelComponentType,
+    };
+  } else {
+    meta_data = await dataStore.item(itemId);
+  }
 
   menuStore.openMenu(
-    itemId,
+    modelId,
     x,
     yUI,
     containerWidth.value,
