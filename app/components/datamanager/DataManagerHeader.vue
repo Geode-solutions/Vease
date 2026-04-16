47112645d8f146ec887297f5cccafc9b3a137e66
SpliiT <spliitdev@gmail.com>
2026-04-16 17:22:50 +0200

    fix(ClassificationStyle): Fix UI problems

--- app/components/datamanager/DataManagerHeader.vue
+++ app/components/datamanager/DataManagerHeader.vue
@@ -36,7 +36,7 @@ defineExpose({ focusSearch: () => searchInput.value?.focus() });
     >
       <v-tab
         value="data"
-        class="text-none font-weight-medium px-6 rounded-t-lg"
+        class="text-none font-weight-medium text-white px-6 rounded-t-lg"
         :variant="activeTab === 'data' ? 'elevated' : 'text'"
         :elevation="activeTab === 'data' ? 4 : 0"
         :border="activeTab === 'data' ? true : false"
@@ -49,7 +49,7 @@ defineExpose({ focusSearch: () => searchInput.value?.focus() });
         v-for="tab in tabs"
         :key="tab.id"
         :value="tab.id"
-        class="text-none font-weight-medium px-6 rounded-t-lg"
+        class="text-none font-weight-medium text-white px-6 rounded-t-lg"
         :variant="activeTab === tab.id ? 'elevated' : 'text'"
         :elevation="activeTab === tab.id ? 4 : 0"
       >
@@ -69,7 +69,9 @@ defineExpose({ focusSearch: () => searchInput.value?.focus() });
         @click="enterPiP"
       >
         <v-icon size="30">mdi-picture-in-picture-bottom-right</v-icon>
-        <v-tooltip activator="parent" location="top">Picture in Picture</v-tooltip>
+        <v-tooltip activator="parent" location="top"
+          >Picture in Picture</v-tooltip
+        >
       </v-btn>
     </v-tabs>
 
