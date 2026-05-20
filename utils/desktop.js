ad231c0f496af4f507e9fbed5b038584ad2aa1de
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-05-20 14:51:14 +0200

    test

--- utils/desktop.js
+++ utils/desktop.js
@@ -40,6 +40,18 @@ async function create_new_window() {
     return { action: "deny" };
   });
 
+  win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+    const responseHeaders = { ...details.responseHeaders };
+    if ((!"Access-Control-Allow-Origin") in details.responseHeaders) {
+      responseHeaders["Access-Control-Allow-Origin"] = ["*"];
+      responseHeaders["Access-Control-Allow-Methods"] = ["*"];
+      responseHeaders["Access-Control-Allow-Headers"] = ["*"];
+    }
+    callback({
+      responseHeaders,
+    });
+  });
+
   console.log("app.isPackaged", app.isPackaged);
   if (app.isPackaged) {
     const serverPath = path.join(process.resourcesPath, "web", "server", "index.mjs");
