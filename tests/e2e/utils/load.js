6445cecc66523041827c19996d5cf1f01e524407
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-06-24 17:42:39 +0200

    layoutImportButton

--- tests/e2e/utils/load.js
+++ tests/e2e/utils/load.js
@@ -7,8 +7,8 @@ async function loadData(window, inputFilename) {
   const inputFileExtension = path.extname(inputFilename);
   console.log("loadData", { inputFilename, inputFileExtension });
   const inputFilePath = path.join(__dirname, "..", "tests", "data", inputFilename);
-  const importButton = await window.getByRole("button", { name: "Import" });
-  await importButton.click();
+  const layoutImportButton = window.getByTestId("layoutImportButton");
+  await layoutImportButton.click();
   const fileInput = window.locator(`input[type="file"][accept*="${inputFileExtension}"]`);
   await fileInput.waitFor({ state: "attached" });
   await fileInput.setInputFiles(inputFilePath);
