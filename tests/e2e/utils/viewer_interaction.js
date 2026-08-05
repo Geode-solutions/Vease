f9cc31094dc0f039f4408a173fc044491c0c8d4d
MaxNumerique <maximilienlandois@gmail.com>
2026-08-05 15:00:04 +0200

    prefer consts and await of click

--- tests/e2e/utils/viewer_interaction.js
+++ tests/e2e/utils/viewer_interaction.js
@@ -70,10 +70,10 @@ async function findOverlappingObjectsPicker(window) {
 
 async function ensureMenuOpen(window, menuTestId) {
   const menuContainer = window.getByTestId(menuTestId);
-  const isAlreadyActive = await menuContainer
-    .getByTestId("activeCircularMenuItemButton")
-    .isVisible();
-  if (!isAlreadyActive) {
+  const activeCircularMenuItemButton = menuContainer.getByTestId(
+    "activeCircularMenuItemButton",
+  );
+  if (!(await activeCircularMenuItemButton.isVisible())) {
     const activeMenuButton = window.getByTestId("activeCircularMenuItemButton");
     if (await activeMenuButton.isVisible()) {
       await activeMenuButton.click();
