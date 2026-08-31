ab0eb7d5eb8edb0905e9331442d272ce4350ae45
SpliiT <106495600+SpliiT@users.noreply.github.com>
2026-08-31 12:00:52 +0000

    Apply prepare changes

--- tests/e2e/tests/polygonal_surface.test.js
+++ tests/e2e/tests/polygonal_surface.test.js
@@ -190,17 +190,14 @@ test("polygons textures", async () => {
   await expect(window).toHaveScreenshot();
 });
 
-test(
-  "quick colormap picker change colormap",
-  async () => {
-    await setMeshPolygonsPolygonAttribute(window, attributeName);
-    const x = 478;
-    const y = 650;
-    await viewerQuickColormap(window, x, y);
-    await setQuickColorMap(window, colorMapName);
-    await expect(window).toHaveScreenshot();
-  },
-);
+test("quick colormap picker change colormap", async () => {
+  await setMeshPolygonsPolygonAttribute(window, attributeName);
+  const x = 478;
+  const y = 650;
+  await viewerQuickColormap(window, x, y);
+  await setQuickColorMap(window, colorMapName);
+  await expect(window).toHaveScreenshot();
+});
 
 test("quick colormap picker change range", async () => {
   await setMeshPolygonsPolygonAttribute(window, attributeName);
