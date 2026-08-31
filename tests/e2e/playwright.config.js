00588c7d9f0af1e29f10579b0dff57b9bf1eeea7
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-08-31 16:55:18 +0200

    fix(Tests): restore all tests

--- tests/e2e/playwright.config.js
+++ tests/e2e/playwright.config.js
@@ -15,7 +15,7 @@ const CI_WORKERS = 2;
 
 const retries = process.env.CI ? CI_RETRIES : 0;
 const workers = process.env.CI ? CI_WORKERS : 3;
-const testMatch = "tests/e2e/tests/grid3d.test.js";
+const testMatch = "tests/e2e/tests/**/*.test.js";
 const maxDiffPixelRatio = 0.02;
 
 const TIMEOUTS = {
