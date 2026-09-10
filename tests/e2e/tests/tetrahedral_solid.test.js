cdc8e0e996e93f90af22bc917cdaf893452552c8
MaxNumerique <maximilienlandois@gmail.com>
2026-09-10 14:02:52 +0200

    update tetrahedral_solid tests, add NaN attribute test  [skip ci]

--- tests/e2e/tests/tetrahedral_solid.test.js
+++ tests/e2e/tests/tetrahedral_solid.test.js
@@ -25,6 +25,7 @@ import {
   openMeshPolyhedraMenu,
   setMeshPolyhedraColorMap,
   setMeshPolyhedraItem,
+  setMeshPolyhedraNoDataColor,
   setMeshPolyhedraPolyhedronAttribute,
   setMeshPolyhedraVertexAttribute,
 } from "@tests/utils/mesh/polyhedra/attribute.js";
@@ -112,6 +113,11 @@ test("vertex attribute", async () => {
     item: 2,
     colorMap: colorMapName,
   });
+  await expect(window).toHaveScreenshot();
+});
+
+test("vertex attribute unmapped elements color", async () => {
+  await setMeshPolyhedraNoDataColor(window);
   await expect(window).toHaveScreenshot();
 });
 
