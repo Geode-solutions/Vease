64b2c8ceb634ba703cb3f32fe59ba62095c17f4d
MaxNumerique <maximilienlandois@gmail.com>
2026-09-10 12:01:15 +0200

    update point_set tests, add NaN attribute test and add attribute swap tests  [skip ci]

--- tests/e2e/utils/mesh/points/attribute.js
+++ tests/e2e/utils/mesh/points/attribute.js
@@ -1,9 +1,19 @@
 import {
+  SLIDER_PINK,
+  clickColorPickerCanvas,
+  clickColorPickerSlider,
+} from "@tests/utils/helpers/color_picker.js";
+import {
+  afterActionWait,
+  ensureMenuOpen,
+  moveMouseOutOfTheWay,
+  openStyleMenu,
+} from "@tests/utils/viewer_interaction.js";
+import {
   meshViewerObjectType,
   pointsFeatureName,
   vertexAttributeType,
 } from "@tests/utils/constants.js";
-import { openStyleMenu } from "@tests/utils/viewer_interaction.js";
 import { setFeatureAttribute } from "@tests/utils/helpers/attribute";
 
 function setMeshPointsVertexAttribute(window, attributeName, options = {}) {
@@ -22,4 +32,23 @@ function openMeshPointsMenu(window) {
   return openStyleMenu(window, menuTestId);
 }
 
-export { openMeshPointsMenu, setMeshPointsVertexAttribute };
+async function setMeshPointsNoDataColor(window) {
+  const menuTestId = `${meshViewerObjectType}${pointsFeatureName}Menu`;
+  await ensureMenuOpen(window, menuTestId);
+  const noDataColorBtn = window.getByTestId("noDataColorBtn").first();
+  await noDataColorBtn.waitFor({ state: "visible" });
+  await noDataColorBtn.click();
+  await window.waitForTimeout(afterActionWait);
+  await window
+    .getByTestId("colorPicker")
+    .filter({ visible: true })
+    .first()
+    .waitFor({ state: "visible" });
+  await clickColorPickerSlider(window, SLIDER_PINK);
+  await clickColorPickerCanvas(window);
+  await noDataColorBtn.click();
+  await moveMouseOutOfTheWay(window);
+  await window.waitForTimeout(afterActionWait);
+}
+
+export { openMeshPointsMenu, setMeshPointsVertexAttribute, setMeshPointsNoDataColor };
