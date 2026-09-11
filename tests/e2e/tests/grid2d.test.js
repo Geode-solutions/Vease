1dea590d8a8e9714fad8cca872e5c8d9de3c43ac
MaxNumerique <maximilienlandois@gmail.com>
2026-09-11 14:04:48 +0200

    grid2d test for color input [skip ci]

--- tests/e2e/tests/grid2d.test.js
+++ tests/e2e/tests/grid2d.test.js
@@ -15,6 +15,13 @@ import {
   viewerContextMenu,
 } from "@tests/utils/viewer_interaction.js";
 import {
+  copyMeshCellsColor,
+  pasteMeshCellsColorInput,
+  setMeshCellsColorInput,
+  setMeshCellsColorWithSlider,
+  setMeshCellsOpacity,
+} from "@tests/utils/mesh/cells/color.js";
+import {
   defaultDataName,
   meshViewerObjectType,
   rgd2dGeodeObjectType,
@@ -28,7 +35,6 @@ import {
   setMeshCellsNoDataColor,
   setMeshCellsVertexAttribute,
 } from "@tests/utils/mesh/cells/attribute.js";
-import { setMeshCellsColorWithSlider, setMeshCellsOpacity } from "@tests/utils/mesh/cells/color.js";
 import { loadData } from "@tests/utils/load.js";
 import { navigateToApp } from "@tests/utils/navigate.js";
 import { setMeshEdgesColorWithSlider } from "@tests/utils/mesh/edges/color.js";
@@ -158,6 +164,21 @@ test("edges color", async () => {
 
 test("cells opacity", async () => {
   await setMeshCellsOpacity(window, cellsOpacity);
+  await expect(window).toHaveScreenshot();
+});
+
+test("cells copy color to clipboard", async () => {
+  await copyMeshCellsColor(window);
+  await expect(window).toHaveScreenshot();
+});
+
+test("cells set color via input", async () => {
+  await setMeshCellsColorInput(window, "0, 255, 0");
+  await expect(window).toHaveScreenshot();
+});
+
+test("cells paste color in input", async () => {
+  await pasteMeshCellsColorInput(window, "0, 0, 255, 0.5");
   await expect(window).toHaveScreenshot();
 });
 
