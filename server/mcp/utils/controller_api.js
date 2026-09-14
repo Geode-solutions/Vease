3706f4ccd0f41e97600c497ad1dcfd1fdb5d95ed
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-09-14 11:13:45 +0200

    fix(OGW): upgrade

--- server/mcp/utils/controller_api.js
+++ server/mcp/utils/controller_api.js
@@ -1,5 +1,5 @@
 // Local imports
-import { getAppBaseUrl } from "@geode/opengeodeweb-front/server/utils/server_config.js";
+import { getAppBaseUrl } from "@ogw_server/utils/server_config";
 
 export async function callControllerApi(path, { method = "POST", headers, body, errorPrefix }) {
   try {
