928f2693bb71846b04c1c5a9f210561637b72a2d
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-09-21 10:54:17 +0200

    let plawright decide workers quantity

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -14,7 +14,7 @@ const CI_RETRIES = 1;
 const CI_WORKERS = 2;
 
 const retries = process.env.CI ? CI_RETRIES : 0;
-const workers = process.env.CI ? CI_WORKERS : 3;
+const workers = process.env.CI ? CI_WORKERS : undefined;
 const testMatch = "tests/e2e/tests/**/*.test.ts";
 const maxDiffPixelRatio = 0.02;
 
