fa64d55c208ac47a5ef3284cf9ddb41142bcd9d4
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-09-23 13:18:33 +0200

    fix(Types): missing typing

--- tests/e2e/utils/data/mesh/points/color.ts
+++ tests/e2e/utils/data/mesh/points/color.ts
@@ -1,3 +1,4 @@
+import type { Locator, Page } from "@playwright/test";
 import { meshViewerObjectType, pointsFeatureName } from "@tests/utils/constants";
 import {
   setFeatureColor,
@@ -8,7 +9,6 @@ import {
   setFeatureOpacity,
   setFeaturePasteColorInput,
 } from "@tests/utils/data/helpers/color";
-import type { Page } from "@playwright/test";
 
 async function setMeshPointsOpacity(window: Page, percent: number): Promise<void> {
   await setFeatureOpacity(window, meshViewerObjectType, pointsFeatureName, percent);
@@ -22,16 +22,19 @@ async function setMeshPointsColorWithSlider(window: Page): Promise<void> {
   await setFeatureColorWithSlider(window, meshViewerObjectType, pointsFeatureName);
 }
 
-function copyMeshPointsColor(window) {
-  return setFeatureCopyColor(window, meshViewerObjectType, pointsFeatureName);
+async function copyMeshPointsColor(window: Page): Promise<void> {
+  await setFeatureCopyColor(window, meshViewerObjectType, pointsFeatureName);
 }
 
-function setMeshPointsColorInput(window, colorText) {
-  return setFeatureColorInput(window, meshViewerObjectType, pointsFeatureName, colorText);
+async function setMeshPointsColorInput(window: Page, colorText: string): Promise<void> {
+  await setFeatureColorInput(window, meshViewerObjectType, pointsFeatureName, colorText);
 }
 
-function pasteMeshPointsColorInput(window, container = window) {
-  return setFeaturePasteColorInput(window, meshViewerObjectType, pointsFeatureName, container);
+async function pasteMeshPointsColorInput(
+  window: Page,
+  container: Page | Locator = window,
+): Promise<void> {
+  await setFeaturePasteColorInput(window, meshViewerObjectType, pointsFeatureName, container);
 }
 
 async function setMeshPointsColoringStyle(window: Page, style: string): Promise<void> {
